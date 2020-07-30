6b8d74b02251ec668ae43101211a7bf95599b8e6
neozhu <new163@163.com>
2020-07-31 07:42:07 +0800

    xx

--- src/AEO.Solution/admin/WebApp/Scripts/miscellaneous/lightgallery/lg-deletebutton.js
+++ src/AEO.Solution/admin/WebApp/Scripts/miscellaneous/lightgallery/lg-deletebutton.js
@@ -1,4 +1,4 @@
-(function (root, factory) {
+﻿(function (root, factory) {
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module unless amdModuleId is set
     define(['jquery'], function (a0) {
