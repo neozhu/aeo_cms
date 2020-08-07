dce7d939b48e730cf7b1c195b43527ffea0d4c0d
neozhu <new163@163.com>
2020-08-07 15:21:34 +0800

    xx

--- src/AEO.Solution/admin/WebApp/App_Helpers/ExceptionExtensions.cs
+++ src/AEO.Solution/admin/WebApp/App_Helpers/ExceptionExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 
 namespace WebApp
@@ -29,15 +30,35 @@ namespace WebApp
                 message = sqlexception.Message;
                 break;
               case 547:   // Constraint check violation
-                message = $"有关联的明细数据存在,不允许删除该记录";
+                var str = sqlexception.Message.Replace("\"", "'");
+                var reg = new Regex(@"The DELETE statement conflicted with the SAME TABLE REFERENCE constraint '(?<index>[\s\S]*?)'. The conflict occurred in database '(?<db>[\s\S]*?)', table '(?<table>[\s\S]*?)', column '(?<column>[\s\S]*?)'.
+The statement has been terminated.");
+                var mc = reg.Match(str);
+                if (mc.Success)
+                {
+                  var table = mc.Groups["table"].Value;
+                  var index = mc.Groups["index"].Value;
+                  message = $"有关联的明细数据存在,不允许删除该记录.在表:{table}上有约束:{index}";
+                }
+                else
+                {
+                  message = $"有关联的明细数据存在,不允许删除该记录";
+                }
                 break;
               case 2601:  // Duplicated key row error
-                var regex = @"\ACannot insert duplicate key row in object \'(?<TableName>.+?)\' with unique index \'(?<IndexName>.+?)\'\. The duplicate key value is \((?<KeyValues>.+?)\)";
+                var regex = @"\ACannot insert duplicate key row in object \'(?<tableName>.+?)\' with unique index \'(?<indexName>.+?)\'\. The duplicate key value is \((?<keyValues>.+?)\)";
                 var match = new System.Text.RegularExpressions.Regex(regex, System.Text.RegularExpressions.RegexOptions.Compiled).Match(sqlexception.Message);
-                var tablename = match?.Groups["TableName"].Value;
-                var indexname = match?.Groups["IndexName"].Value;
-                var keyvalue = match?.Groups["KeyValues"].Value;
-                message = $"[{keyvalue}] 已经存在,不允许重复.索引:{indexname},表:{tablename}";
+                if (match.Success)
+                {
+                  var tablename = match?.Groups["tableName"].Value;
+                  var indexname = match?.Groups["indexName"].Value;
+                  var keyvalue = match?.Groups["keyValues"].Value;
+                  message = $"值:{keyvalue} 已经存在表 {tablename} 中已存在,不允许重复插入,约束:{indexname}";
+                }
+                else
+                {
+                  message = $"不允许重复插入相同的数据";
+                }
                 break;
               default:
                 message = e.InnerException.InnerException.Message;
